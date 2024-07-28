f73e8c530cfe3aac23c51eb60ef3245185651ae7
Yong Zhou <zyong06@gmail.com>
2024-07-28 09:57:53 +0800

    [update] boost and yaml

--- buildroot/package/yaml-cpp/yaml-cpp.mk
+++ buildroot/package/yaml-cpp/yaml-cpp.mk
@@ -4,15 +4,17 @@
 #
 ################################################################################
 
-YAML_CPP_VERSION = 0.5.2
-YAML_CPP_SITE = $(call github,jbeder,yaml-cpp,release-$(YAML_CPP_VERSION))
+YAML_CPP_VERSION = 0.8.0
+YAML_CPP_SITE = $(call github,jbeder,yaml-cpp,$(YAML_CPP_VERSION))
 YAML_CPP_INSTALL_STAGING = YES
 YAML_CPP_LICENSE = MIT
-YAML_CPP_LICENSE_FILES = license.txt
-
-YAML_CPP_DEPENDENCIES = boost
+YAML_CPP_LICENSE_FILES = LICENSE
+YAML_CPP_CPE_ID_VALID = YES
 
 # Disable testing and parse tools
-YAML_CPP_CONF_OPTS += -DYAML_CPP_BUILD_TOOLS=OFF
+YAML_CPP_CONF_OPTS += \
+	-DCMAKE_DEBUG_POSTFIX="" \
+	-DYAML_CPP_BUILD_TESTS=OFF \
+	-DYAML_CPP_BUILD_TOOLS=OFF
 
 $(eval $(cmake-package))
